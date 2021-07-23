9893920a8e9a2adbcaaa36f1405c6829c62ebced
Kalim Ullah <kaleembtni@gmail.com>
2021-07-23 15:56:35 +0500

    Complete step 1 form

--- src/components/jobs/create/index.js
+++ src/components/jobs/create/index.js
@@ -1,4 +1,4 @@
-import { Card } from "antd";
+import { Card, Form, Button, Input, Row, Col } from "antd";
 import { Link, useParams } from "react-router-dom";
 
 import "./index.scss";
@@ -31,8 +31,104 @@ const CreateJob = () => {
         </div>
 
         <div className="form-area">
-          This is form area
-          <Link to={`/jobs/create/${Number(id) + 1}`}>Next</Link>
+          <Form name="basic" initialValues={{ remember: true }}>
+            <Row gutter={32} className="reach-row">
+              <Col span={12}>
+                <Form.Item
+                  label="Looking For"
+                  name="job_title"
+                  rules={[{ required: true, message: "JOb title is required" }]}
+                >
+                  <Input
+                    className="reach-first-input"
+                    placeholder="Enter Job title"
+                  />
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item
+                  label="Experience"
+                  name="experience"
+                  rules={[
+                    { required: true, message: "Experience is required" },
+                  ]}
+                >
+                  <Input
+                    className="reach-first-input"
+                    placeholder="Experience"
+                  />
+                </Form.Item>
+              </Col>
+            </Row>
+
+            <Row gutter={32} className="reach-row">
+              <Col span={12}>
+                <Form.Item
+                  label="Education"
+                  name="education"
+                  rules={[
+                    { required: true, message: "Education is required!" },
+                  ]}
+                >
+                  <Input
+                    className="reach-first-input"
+                    placeholder="Education..."
+                  />
+                </Form.Item>
+              </Col>
+            </Row>
+
+            <Row gutter={32} className="reach-row">
+              <Col span={24}>
+                <Form.Item
+                  label="Skills"
+                  name="skills"
+                  rules={[{ required: true, message: "Skills required" }]}
+                >
+                  <Input className="reach-first-input" placeholder="Skills" />
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={32} className="reach-row">
+              <Col span={24}>
+                <Form.Item
+                  label="Description"
+                  name="desciption"
+                  rules={[
+                    { required: true, message: "Description is required" },
+                  ]}
+                >
+                  <Input.TextArea
+                    className="reach-first-input"
+                    placeholder="desciption"
+                    rows={4}
+                  />
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={32} className="reach-row">
+              <Col span={24}>
+                <Form.Item
+                  name="template"
+                  rules={[{ required: true, message: "Skills required" }]}
+                >
+                  <Input
+                    type="file"
+                    className="file-upload"
+                    placeholder="File"
+                  />
+                </Form.Item>
+              </Col>
+            </Row>
+          </Form>
+        </div>
+        <div className="card-actions">
+          <Button size="large" className="card-btn">
+            <Link to={`/jobs/create/${Number(id) - 1}`}>PREVIOUS</Link>
+          </Button>
+          <Button type="primary" size="large" className="card-btn">
+            <Link to={`/jobs/create/${Number(id) + 1}`}>NEXT</Link>
+          </Button>
         </div>
       </Card>
     </div>
